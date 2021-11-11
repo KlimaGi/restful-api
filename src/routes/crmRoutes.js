7f2f13d0920439d7e7d55144e9040c5f78acf60c
Giedre <giedre.klima@gmail.com>
2021-11-11 23:45:28 +0200

    get with id, put endpoints

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,24 +1,28 @@
-import { addNewContact } from "../controllers/crmController";
+import {
+  addNewContact,
+  getContacts,
+  getContactWithID,
+  updateContact,
+} from "../controllers/crmController";
 
 const routes = (app) => {
   app
     .route("/contact")
-    .get(
-      (req, res, next) => {
-        // middleware
-        console.log(`Request from: ${req.originalUrl}`);
-        console.log(`Request type: ${req.method}`);
-        next();
-      },
-      (req, res, next) => {
-        res.send("GET request successful!");
-      }
-    )
+    .get((req, res, next) => {
+      // middleware
+      console.log(`Request from: ${req.originalUrl}`);
+      console.log(`Request type: ${req.method}`);
+      next();
+    }, getContacts)
+
     .post(addNewContact);
 
   app
     .route("/contact/:contactID")
-    .put((req, res) => res.send("PUT request succesful!"))
+    .get(getContactWithID)
+
+    .put(updateContact)
+
     .delete((req, res) => res.send("DELETE request succesful!"));
 };
 
